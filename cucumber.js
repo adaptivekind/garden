c82739258b6c5f20d0ce33120769866ebb2daf7a
Ian Homer <code@ianhomer.com>
2025-08-10 21:24:56 +0100

    test: move e2e tests into test folder

--- cucumber.js
+++ cucumber.js
@@ -1,8 +1,8 @@
 module.exports = {
   default: {
     requireModule: ["ts-node/register"],
-    require: ["e2e/steps/**/*.ts"],
-    format: ["progress", "html:e2e/reports/cucumber-report.html"],
+    require: ["test/e2e/steps/**/*.ts"],
+    format: ["progress", "html:test/e2e/reports/cucumber-report.html"],
     formatOptions: {
       snippetInterface: "async-await",
     },
