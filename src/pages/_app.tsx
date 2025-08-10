f3d454b57cb5bf3df5487ab502ff54f5430a2d26
Ian Homer <code@ianhomer.com>
2025-08-10 17:12:54 +0100

    style: add favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -25,6 +25,11 @@ export default function App({ Component, pageProps }: CustomAppProps) {
           content="View markdown files in your directory"
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="icon" type="image/svg+xml" sizes="16x16" href="/favicon-16x16.svg" />
+        <link rel="icon" type="image/svg+xml" sizes="32x32" href="/favicon-32x32.svg" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon.svg" />
       </Head>
       {hasGraph && (
         <header style={{ 
